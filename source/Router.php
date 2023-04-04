f90f3366a7353d086c0b61a351d96f2adfc33553
elenroart <>
2023-04-04 19:52:37 +0300

    0.4.3

--- source/Router.php
+++ source/Router.php
@@ -4,19 +4,20 @@ namespace Source;
 use Source\Exception\BadRouteException;
 
 class Router{
-    private static $routes = [];
-    private static $routeNotFound = false;
-    private static $methodNotFound = false;
-    private static $classNotFound = false;
-    private static $pathNotFound = false;
-    private static $httpMethodNotAllowed = false;
+    private $routes = [];
+    private $exception = "";
+
+    public function __construct($routes = [],$exception=''){
+        $this->routes = $routes;
+        $this->exception = $exception;
+    }
 
     public function addRoute($httpMethod,$route,$actionData)
     {        
         $class = $actionData[0];
         $method = $actionData[1];
         
-        self::$routes[] = 
+        $this->routes[] = 
         [
             'httpMethod' => $httpMethod,
             'route' => $route,
@@ -29,7 +30,9 @@ class Router{
         $uri = $this->getURI();
         $params = [];
 
-        foreach (self::$routes as $route) {
+        foreach ($this->routes as $route) {
+
+            $this->exception = '';
 
             if($_SERVER['REQUEST_METHOD'] == $route['httpMethod'])
             {
@@ -38,14 +41,12 @@ class Router{
                     $route['route'] = '/'.$route['route'];
                 }
 
-                self::$httpMethodNotAllowed = false;
 
                 $pattern = preg_replace('/\//', '\\/', $route['route']);
                 $pattern = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[^\/]+)', $pattern);             
                 $pattern = '/^' . $pattern . '$/';
                 
                 if (preg_match($pattern, $uri, $matches)) {
-                    self::$routeNotFound = false;
 
                     $filePath = $route['class'].'.php';
                     
@@ -57,63 +58,42 @@ class Router{
                     
                     if(file_exists($filePath))
                     {
-                        self::$pathNotFound = false;
-
                         include $filePath;
 
                         if(class_exists($route['class'],false))
                         {
-                            self::$classNotFound = false;
                             if(method_exists($route['class'],$route['method']))
                             {
-                                self::$methodNotFound = false;
                                 $object = new $route['class'];
 
                                 call_user_func_array([$object,$route['method']],$params);
 
-                                return true;
+                                return;
                             }else{                      
-                                self::$methodNotFound = true;
+                                $this->exception="methodNotFound";
                             }
                         }else{
-                            self::$classNotFound = true;
+                            $this->exception="classNotFound";
                         }
                     }else{
-                        self::$pathNotFound = true;
+                        $this->exception="pathNotFound";
                     }                
                 }
-                else{
-                    self::$routeNotFound = true;                 
+                else{     
+                    $this->exception="routeNotFound";           
                 }
             }
             else{
-                self::$httpMethodNotAllowed = true;
-                
+                $this->exception="httpMethodNotAllowed";
             }
         }
         $this->handlerException();
     }
     private function handlerException()
     {
-        if(self::$httpMethodNotAllowed === true)
+        if($this->exception != '')
         {
-            throw new BadRouteException('HttpMethod not Allowed!');
-        }
-        if(self::$routeNotFound === true)
-        {
-            throw new BadRouteException('Route not found!');
-        }
-        if(self::$pathNotFound === true)
-        {
-            throw new BadRouteException('file not found!');
-        }
-        if(self::$classNotFound === true)
-        {
-            throw new BadRouteException('class not found!');
-        }
-        if(self::$methodNotFound === true)
-        {
-            throw new BadRouteException('Method not found!');
+            throw new BadRouteException($this->exception);
         }
     }
     private function getURI()
