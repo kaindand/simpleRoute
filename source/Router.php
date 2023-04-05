3b4f6e5f3a70f59f1ee499428f54464e13ffae99
Петр <70967595+kaindand@users.noreply.github.com>
2023-04-05 22:43:37 +0300

    0.5

--- source/Router.php
+++ source/Router.php
@@ -6,10 +6,12 @@ use Source\Exception\BadRouteException;
 class Router{
     private $routes = [];
     private $exception = "";
+    private $params = [];
 
-    public function __construct($routes = [],$exception=''){
+    public function __construct($routes = [],$params = [],$exception=''){
         $this->routes = $routes;
         $this->exception = $exception;
+        $this->params = $params;
     }
 
     public function addRoute($httpMethod,$route,$actionData)
@@ -25,75 +27,83 @@ class Router{
             'method' => $method,          
         ];
     }
-    public function run()
+    public function dispatch()
     {
-        $uri = $this->getURI();
-        $params = [];
-
         foreach ($this->routes as $route) {
 
             $this->exception = '';
 
-            if($_SERVER['REQUEST_METHOD'] == $route['httpMethod'])
+            if(substr($route['route'],0,1) != '/')
             {
-                if(substr($route['route'],0,1) != '/')
-                {
-                    $route['route'] = '/'.$route['route'];
-                }
-
-
-                $pattern = preg_replace('/\//', '\\/', $route['route']);
-                $pattern = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[^\/]+)', $pattern);             
-                $pattern = '/^' . $pattern . '$/';
-                
-                if (preg_match($pattern, $uri, $matches)) {
-
-                    $filePath = $route['class'].'.php';
-                    
-                    foreach ($matches as $key => $value) {
-                        if (is_string($key)) {
-                            $params[$key] = $value;
-                        }
-                    }
-                    
-                    if(file_exists($filePath))
-                    {
-                        include $filePath;
-
-                        if(class_exists($route['class'],false))
-                        {
-                            if(method_exists($route['class'],$route['method']))
-                            {
-                                $object = new $route['class'];
-
-                                call_user_func_array([$object,$route['method']],$params);
-
-                                return;
-                            }else{                      
-                                $this->exception="methodNotFound";
-                            }
-                        }else{
-                            $this->exception="classNotFound";
-                        }
-                    }else{
-                        $this->exception="pathNotFound";
-                    }                
-                }
-                else{     
-                    $this->exception="routeNotFound";           
-                }
+                $route['route'] = '/'.$route['route'];
             }
-            else{
-                $this->exception="httpMethodNotAllowed";
-            }
+
+
+            $pattern = preg_replace('/\//', '\\/', $route['route']);
+            $pattern = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[^\/]+)', $pattern);             
+            $pattern = '/^' . $pattern . '$/';
+
+            $this->execution($route,$pattern);
         }
         $this->handlerException();
+    }
+    private function execution($route,$pattern)
+    {
+        $uri = $this->getURI();
+        
+        if($_SERVER['REQUEST_METHOD'] == $route['httpMethod'])
+        {          
+            if (preg_match($pattern, $uri, $matches)) {
+
+                $filePath = $route['class'].'.php';
+                
+                $this->addParameters($matches);
+
+                if(file_exists($filePath))
+                {
+                    include $filePath;
+
+                    if(class_exists($route['class'],false))
+                    {
+                        if(method_exists($route['class'],$route['method']))
+                        {
+                            $object = new $route['class'];
+
+                            call_user_func_array([$object,$route['method']],$this->params);
+
+                            exit;
+                        }else{                      
+                            $this->exception="methodNotFound";
+                        }
+                    }else{
+                        $this->exception="classNotFound";
+                    }
+                }else{
+                    $this->exception="pathNotFound";
+                }                
+            }
+            else{     
+                $this->exception="routeNotFound";           
+            }
+        }
+        else{
+            $this->exception="httpMethodNotAllowed";
+        }
+        return;
     }
     private function handlerException()
     {
         if($this->exception != '')
         {
             throw new BadRouteException($this->exception);
+        }
+    }
+    public function addParameters($matches)
+    {
+        foreach ($matches as $key => $value) {
+            if (is_string($key)) {
+                $this->params[$key] =  $value;
+            }
         }
     }
     private function getURI()
