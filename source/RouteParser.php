94984a0dfcd28bac1bd763919c66cb65c5c7aee5
Петр <70967595+kaindand@users.noreply.github.com>
2023-05-06 19:06:56 +0300

    routeParser

--- source/RouteParser.php
+++ source/RouteParser.php
@@ -4,45 +4,40 @@ namespace SimpleRoute;
 
 class RouteParser
 {
-    private $dataRoute;
-
-    public function __construct(array $dataRoute = [])
-    {
-        $this->dataRoute = $dataRoute;
-    }
-    public function parse($route,$prefix = '')
+    public function parse($route, $prefix = '')
     {
         $route = $prefix.$route;
 
-        if(substr($route,0,1) != '/')
-        {
+        if(substr($route, 0, 1) != '/') {
             $route = '/'.$route;
         }
 
-        $route = preg_replace('/\//', '\\/', $route);
-        $route = preg_replace('/\{([a-z]+)?\}/', '(?P<\1>[^\/]+)',$route);             
-        $route = '/' . $route. '/';  
+        $pattern = preg_replace('/\//', '\\/', $route);
 
-        $dataRoute['route'] = $route;
+        $pattern = preg_replace_callback('/\{([a-z]+)?\}/', function ($matches) {
+            $argument = $matches[1];
+            $replace = '[^}]+';
+            return '(?P<'.$argument.'>'.$replace.')';
+        }, $pattern);
 
-        $parameters = $this->setParameters($route);
+        $pattern = '/'.$pattern.'/';
+
+        $dataRoute['route'] = $pattern;
+
+        $parameters = [];
+
+        if (preg_match($pattern, $_SERVER['REQUEST_URI'], $matches)) {
+            foreach ($matches as $key => $value) {
+
+                if (is_string($key)) {
+                    $parameters[$key] =  $value;
+                }
+            }
+        }
 
         $dataRoute['parameters'] = $parameters;
 
         return $dataRoute;
     }
 
-    private function setParameters($route)
-    {
-        if (preg_match($route, $_SERVER['REQUEST_URI'], $matches)) {   
-            foreach ($matches as $key => $value) {
-                
-                if (is_string($key)) {
-                    $parameters[$key] =  $value;
-                }
-            }
-            return $parameters;
-        }
-        return null;
-    }
 }
